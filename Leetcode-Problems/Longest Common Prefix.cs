ed496f5460d57ae435afcb6ab47270f832cc0e86
Nishchay Tamhankar <nishchay@compdhenu.com>
2023-11-06 19:23:08 +0530

    longest common prefix: remove last character until a match is found ( doesn't work always )

--- Leetcode-Problems/Longest Common Prefix.cs
+++ Leetcode-Problems/Longest Common Prefix.cs
@@ -4,20 +4,15 @@
     {
         public string Run(string[] strs)
         {
-            string p = string.Empty;
+            string p = strs[0];
 
+            // for every character in string[i], remove last character until string[i] ends
             for (int sI = 0; sI < strs.Length; sI++)
-            {
-                if (!strs[sI].StartsWith(p) && p.Any())
-                    p = p[..^1];
-
-                if (sI != strs.Length - 1)
-                    for (int cI = 0; cI < strs[sI].Length; cI++)
-                    {
-                        if (strs[sI][cI] == strs[sI + 1][cI])
-                            p += strs[sI][cI];
-                    }
-            }
+                for (int cI = 0; cI < strs[sI].Length; cI++)
+                {
+                    if (!strs[sI].StartsWith(p))
+                        p = p[..^1];
+                }
 
             return p;
         }
